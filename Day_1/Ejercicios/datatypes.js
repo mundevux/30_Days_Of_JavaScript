16df76f983e13c4e36ff1e2e4732ca2b93884426
Camilo Cuitiva <camiloandres174@gmail.com>
2022-08-30 10:51:14 -0500

    Dia 1 de 30 Dias de JavaScript

---
+++ Day_1/Ejercicios/datatypes.js
@@ -0,0 +1,7 @@
+// 5. Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+
+console.log(typeof nombre)
+console.log(typeof edad)
+console.log(typeof estaCasado)
+console.log(typeof tieneHijos)
+console.log(typeof tieneMascota)
